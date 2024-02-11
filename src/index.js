c989e792fffc91524a80e12b756b355b764616b5
sheymantech <olaniyansheyi1704@gmail.com>
2024-02-11 17:15:20 +0100

    new feature

--- src/index.js
+++ src/index.js
@@ -68,6 +68,11 @@ function Menu() {
   );
 }
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   return (
     <footer>{new Date().toLocaleTimeString()} we are currently open</footer>
   );
