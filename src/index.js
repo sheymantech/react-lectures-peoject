182660d05d7e87e043671edce6de303dc52e030d
sheymantech <olaniyansheyi1704@gmail.com>
2024-02-20 00:02:52 +0100

    new feature

--- src/index.js
+++ src/index.js
@@ -148,3 +148,39 @@ root.render(
     <App />
   </React.StrictMode>
 );
+//  code below is not from this project/
+function Form() {
+  const [description, setDescription] = useState("");
+  const [nameTag, setNameTag] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const menuItem = { description, nameTag, packed: false, id: Date.now() };
+
+    // initialItems.push(menuItem);
+    // console.log(initialItems);
+
+    setDescription("");
+    setNameTag(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={nameTag} onChange={(e) => setNameTag(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num}>{num}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
